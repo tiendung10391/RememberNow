b324a9dc87e3b08dade8b022d225ab14179aa516
Nguyen Tien Dung <tiendung10391@gmail.com>
2015-06-18 13:40:27 +0700

    remember now

--- gen/ITPSoft/app/remembernow/BuildConfig.java
+++ gen/ITPSoft/app/remembernow/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package ITPSoft.app.remembernow;
+package itpsoft.app.remembernow;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
